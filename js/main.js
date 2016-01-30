b885063a4559481015245eed77d17c04ae49b87a
Marin Atanasov <tyxla@abv.bg>
2016-01-30 13:17:53 +0200

    implementing admin js functionality, with backwards compatibility

--- js/main.js
+++ js/main.js
@@ -20,3 +20,43 @@ function GFCB_InsertFieldGroup(index){
     LoadFieldChoices(field);
     UpdateFieldChoices(GetInputType(field));
 }
+
+jQuery(function($) {
+
+	// hooking to field choices loading
+	$(document).on('gform_load_field_choices', function(event, field) {
+
+		// should be disabled for non-select fields
+		if (field.type != 'checkbox') {
+			return false;
+		}
+
+		setTimeout(function() {
+
+			field = GetSelectedField();
+
+			// going through all select options in the admin
+			$('#field_' + field.id + ' #field_choices > li').each(function(idx) {
+				// preparing group
+				if (field.choices[idx].isCheckboxGroup) {
+					$(this).addClass('is_checkbox_group'); 
+					$(this).find('.gfield_choice_checkbox').attr('disabled', 'disabled');
+				}
+
+				// newer GF version button
+				var newer = $(this).find('.gf_insert_field_choice');
+				if (newer.length) {
+					$('<a class="gf_insert_field_group" onclick="GFCB_InsertFieldGroup(' + (idx+1) + ');"><i class="fa fa-caret-square-o-down"></i></a>').insertAfter(newer);
+				}
+
+				// older GF version button (compatibility mode)
+				var older = $(this).find('.add_field_choice');
+				if (older.length) {
+					$('<img src="' + window.gfcb_plugin_url + '/images/icon-drop-list.png" class="add_field_group" title="add a group" alt="add a group" onclick="GFCB_InsertFieldGroup(' + (idx+1) + ');">').insertAfter(older);
+				}
+			});
+		}, 20);
+
+	});
+
+});
